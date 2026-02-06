c742989f4654d82a809bbf1c54ea85fa173cbcce
unknown <panhao1@ramaxel.local>
2026-02-06 16:07:31 +0800

    feat:给simple_example添加控制台输出

--- examples/simple_example.cc
+++ examples/simple_example.cc
@@ -21,7 +21,8 @@ using ROCKSDB_NAMESPACE::WriteOptions;
 #if defined(OS_WIN)
 std::string kDBPath = "C:\\Windows\\TEMP\\rocksdb_simple_example";
 #else
-std::string kDBPath = "/tmp/rocksdb_simple_example";
+//std::string kDBPath = "/tmp/rocksdb_simple_example";
+std::string kDBPath = "my_rocksdb_db";
 #endif
 
 int main() {
